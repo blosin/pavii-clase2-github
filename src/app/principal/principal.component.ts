27191289f03918de318ece752f99b815368f9fe9
blosin <ocampo_joel@outlook.com>
2020-06-15 16:18:42 -0300

    2 files modified

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -9,11 +9,19 @@ export class PrincipalComponent implements OnInit {
 
   materia="PAVII";
   validador=true;
+  mensajeBoton="Agregar";
   imagen1='https://www.frc.utn.edu.ar/imagenes/logoutn.gif';
+  color='red';
   constructor() { }
   
 
   ngOnInit() {
+  }
+  metodoClick(){
+    this.materia+="boton agregando";
+    this.mensajeBoton="ya se agrego";
+
+
   }
 
 }
