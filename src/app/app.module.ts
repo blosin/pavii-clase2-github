5f53471776a2d7636558881e35036243587db0a1
blosin <ocampo_joel@outlook.com>
2020-06-15 15:51:22 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { MenuComponent } from './menu/menu.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ MenuComponent ],
-  bootstrap:    [ MenuComponent ]
+  declarations: [ MenuComponent, PrincipalComponent ],
+  bootstrap:    [ PrincipalComponent ]
 })
 export class AppModule { }
